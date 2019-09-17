3028cf487632aaa0adef676499ea8e841b183667
Jacky Lau <jackylau0126@gmail.com>
2019-09-17 02:42:26 +0000

    Improve

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😎😎😎Assertion Passed: " + actual + " === " + expected);
+    console.log(`😎😎😎Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("☹️☹️☹️Assertion Failed: " + actual + " != " + expected);
+    console.log(`☹️☹️☹️Assertion Failed: ${actual} != ${expected}`);
   }
 };
 
