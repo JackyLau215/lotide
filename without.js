01d2f9d230c2986d694069198def52c3cf95dc18
Jacky Lau <jackylau0126@gmail.com>
2019-09-18 23:50:41 +0000

    without

---
+++ without.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎😎Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☹️☹️☹️Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😎😎😎Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☹️☹️☹️Assertion Failed: ${actual} != ${expected}`);
+  };
+};
+
+const without = function (original, removedItems) {
+  let result = [];
+  for (let i = 0; i < original.length; i++) {
+    let foundItem = false;
+    for (let j = 0; j < removedItems.length; j++) {
+      if (original[i] === removedItems[j]){
+        foundItem = true
+      }
+    }
+    if(!foundItem) {
+      result.push(original[i]);
+    }
+  }
+  return result;
+}
+
+// TEST CODE
+let actual = without([1, 2, 3], [1])
+assertArraysEqual(actual, [2,3]);
+
+actual = without(["1", "2", "3"], [1, 2, "3"])
+assertArraysEqual(actual, ["1", "2"]);
