ff00c76f5625d83b9f324733e455fdb643f5a4f6
Jacky Lau <jackylau0126@gmail.com>
2019-09-19 20:01:47 +0000

    letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,52 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎😎Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☹️☹️☹️Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😎😎😎Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☹️☹️☹️Assertion Failed: ${actual} != ${expected}`);
+  };
+};
+
+const letterPositions = function(phrase) {
+  let phraseArray = phrase.split("");
+  phrase = phrase.replace(/ /g, "");
+  const results = {};
+  for (let i = 0; i < phraseArray.length; i++) {
+    const letter = phraseArray[i];
+    if (letter === " ") {
+      // Do nothing
+    } else if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
+  }
+  return results;
+};
+
+
+// TEST CODE
+console.log(letterPositions("lighthouse in the house"));
+
+assertArraysEqual(letterPositions("lighthouse in the house").l, [0]);
+
+
+
