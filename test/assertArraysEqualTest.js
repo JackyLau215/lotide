002b8d9a5afd907d0f8c0cc728a8a0cd96ba8080
Jacky Lau <jackylau0126@gmail.com>
2019-09-23 22:13:18 +0000

    updated

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+
+// TEST CODE
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
